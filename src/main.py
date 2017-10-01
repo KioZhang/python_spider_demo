e426cd4d8a01520bed94995c900ff1d7e27e7297
zhangyu <zhangyu@ccclubs.com>
2017-10-01 15:55:47 +0800

    daily update

--- src/main.py
+++ src/main.py
@@ -3,3 +3,16 @@
 # @Time    : 2017-10-01 14:56
 # @Author  : Kio
 # @Software: PyCharm
+
+import urllib.request
+url = "http://www.douban.com/"
+webPage = urllib.request.urlopen(url)
+data = webPage.read()
+data = data.decode('UTF-8')
+print(data)
+print(type(webPage))
+print(webPage.geturl())
+# print(webPage.info())
+# print(webPage.getcode())
+
+
